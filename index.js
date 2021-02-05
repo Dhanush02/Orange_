55ba202c5227d955abb83930a15e3a2cb8fe2da0
Dhanush Karthick <dhanukarthick15@gmail.com>
2021-02-05 21:40:55 +0530

    result

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ const generateMail = (
     host: 'smtp.gmail.com',
     port: 587,
     secure: false, // true for 465, false for other ports
-    auth: {
-      user: process.env.user,
-      pass: process.env.pass,
-    },
+    auth: false,
     
   });
 
